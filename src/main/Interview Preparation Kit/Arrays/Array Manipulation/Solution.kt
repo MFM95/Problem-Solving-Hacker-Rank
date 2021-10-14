a32c86481e7b011e3ff2706932e252408ec35070
MFM95 <mohamed.fawzy9876@gmail.com>
2021-10-14 13:31:24 +0200

    remove unused imports

--- src/main/Interview Preparation Kit/Arrays/Array Manipulation/Solution.kt
+++ src/main/Interview Preparation Kit/Arrays/Array Manipulation/Solution.kt
@@ -1,21 +1,4 @@
-import java.io.*
-import java.math.*
-import java.security.*
-import java.text.*
-import java.util.*
-import java.util.concurrent.*
-import java.util.function.*
-import java.util.regex.*
-import java.util.stream.*
-import kotlin.collections.*
-import kotlin.comparisons.*
-import kotlin.io.*
-import kotlin.jvm.*
-import kotlin.jvm.functions.*
-import kotlin.jvm.internal.*
-import kotlin.ranges.*
-import kotlin.sequences.*
-import kotlin.text.*
+
 
 fun arrayManipulation(n: Int, queries: Array<Array<Int>>): Long {
     val arr = LongArray(n+1)
